cf2e2599e796a76cd0704dca22ddf3b75bfc829b
laurenstephen-s <65381952+laurenstephen-s@users.noreply.github.com>
2020-06-23 01:10:23 -0400

    Create Final Assignment.py

---
+++ Final Assignment.py
@@ -0,0 +1,8 @@
+##  This is my final assignment for the Analyzing Data With Python course from IBM. In this assignment, I analyzed and predicted 
+##  alcohol consumption by country using attributes or features such as beer servings and wine servings
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
